a0a17f766edd71b72bad9297a2e68b0c4e798e56
DustyShap <dustinshapiro@gmail.com>
2018-02-22 08:45:43 -0600

    remove comments

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 from flask_uploads import UploadSet, configure_uploads, AUDIO
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-#from models import *
 import os
 import csv
 import pytz
@@ -17,22 +16,6 @@ audio = UploadSet('audio', AUDIO)
 app.config['UPLOADED_AUDIO_DEST'] = os.environ['UPLOAD_PATH']
 configure_uploads(app, audio)
 
-# def logger(play_type,search_term):
-#     with open(os.environ['HOME_PATH'] + 'logs.csv','a') as searchFile:
-#         searchFileWriter = csv.writer(searchFile)
-#         searchFileWriter.writerow([play_type,search_term,datetime.datetime.now(
-#             pytz.timezone('America/Chicago')).strftime("%A, %d. %B %Y %I:%M%p")])
-#         searchFile.close()
-
-
-# @app.before_request
-# def before_request():
-#     initialize_db()
-
-
-# @app.teardown_request
-# def teardown_request(exception):
-#     db.close()
 
 
 @app.route('/')
@@ -53,38 +36,10 @@ def upload():
         {"filename":filename,"speaker":speaker,"tags":tags,"transcription":transcription})
 
     db.commit()
-    # Drops.create(
-    #     filename=filename,
-    #     speaker=speaker,
-    #     tags=tags,
-    #     transcription=transcription
-    # )
 
     return jsonify({'file': filename})
 
 
-# @app.route('/count', methods=['GET', 'POST'])
-# def count():
-#
-#     filename = request.form['filename']
-#     element = request.form['element']
-#
-#     if filename != 'CLIP%20THAT%20OFF.mp3':
-#
-#         logger('Audio Clicked',filename)
-#
-#         c = Drops.select().where(Drops.filename == filename)
-#         for drop in c:
-#             dropcount = drop.return_count()
-#             if dropcount:
-#                 q = Drops.update(count=Drops.count +
-#                                  1).where(Drops.filename == filename)
-#             else:
-#                 q = Drops.update(count=1).where(Drops.filename == filename)
-#
-#             q.execute()
-#
-#     return jsonify({'filename': filename})
 
 
 @app.route('/process', methods=['POST', 'GET'])
@@ -117,42 +72,20 @@ def process():
 
     return jsonify({"drops": process_drop_results(drops)})
 
-    #
-    # def to_dict(drops):
-    #     return {
-    #         'filename': drops.filename,
-    #         'speaker': drops.speaker,
-    #         'transcription': drops.transcription.upper()[0:110]
-    #         }
-    #
-    #
-    # #Function this
-    # for drop in drops:
-    #     drop_as_dict = to_dict(drop)
-    #     drops_list.append(drop_as_dict)
-    #
-    # return jsonify({"drops":drops_list})
 
 
-@app.route('/swope')
-def swope():
-    return render_template('swope.html')
-
-
-@app.route('/swopeprocess', methods=['GET', 'POST'])
-def swope_process():
-
-    keyword = request.form['keyword'].lower().strip()
-    drops = Drops.select().where(Drops.transcription.contains(keyword))
-
-    drops_as_list = []
-
-    if drops:
-        for drop in drops:
-            drop_as_dict = drop.as_dict()
-            drops_as_list.append(drop_as_dict)
-        return jsonify({'keyword': drops_as_list})
-    return jsonify({'keyword': drops_as_list})
+# @app.route('/swope')
+# def swope():
+#     return render_template('swope.html')
+#
+#
+# @app.route('/swopeprocess', methods=['GET', 'POST'])
+# def swope_process():
+#
+#     keyword = request.form['keyword'].lower().strip()
+#     drops = Drops.select().where(Drops.transcription.contains(keyword))
+#     return jsonify({"keyword":process_drop_results(drops)})
+#
 
 
 def add_wildcard(string):
