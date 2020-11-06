23916da092423951901649ff8d3ea8b58f643b4f
Dustin Shapiro <dustinshapiro@gmail.com>
2020-11-06 10:53:42 -0600

    Drop User table

--- models.py
+++ models.py
@@ -29,12 +29,6 @@ class Drop(db.Model):
 
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
-    password = db.Column(db.String, nullable=False)
-
 class AdminUser(db.Model):
     __tablename__ = 'admin_user'
     id = db.Column(db.Integer, primary_key=True)
